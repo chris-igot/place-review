73c9519e69da850301d7d51d9483ccd415daa1b4
Christiani Igot <c.bensi.igot@gmail.com>
2022-06-13 19:13:54 -0400

    Streamlined external api calls

--- backend/src/controllers/PlaceController.ts
+++ backend/src/controllers/PlaceController.ts
@@ -2,39 +2,14 @@ import { Request, Response } from "express";
 import {
     Client,
     PlaceAutocompleteResult,
-    PlaceData,
 } from "@googlemaps/google-maps-services-js";
 import Place from "../entities/Place";
 import { AppDataSource } from "../data-source";
-import { MetaData } from "metadata-scraper/lib/types";
-import getWebsiteInfo from "../utilities/getWebsiteInfo";
 import User from "../entities/User";
 import FavoriteRef from "../entities/FavoriteReference";
 import { DeleteResult } from "typeorm";
 
-const detailFields = [
-    "adr_address",
-    "business_status",
-    "formatted_address",
-    "geometry",
-    "icon",
-    "icon_mask_base_uri",
-    "icon_background_color",
-    "name",
-    "place_id",
-    "plus_code",
-    "type",
-    "url",
-    "utc_offset",
-    "vicinity",
-    "formatted_phone_number",
-    "international_phone_number",
-    "opening_hours",
-    "website",
-    "price_level",
-    "rating",
-    "user_ratings_total",
-];
+const detailFields = ["formatted_address", "name", "place_id"];
 
 const client = new Client({});
 
@@ -54,18 +29,6 @@ export async function getAutocompleteResult(req: Request, res: Response) {
             })
             .then(async (response) => {
                 result = response.data.predictions;
-
-                for (let i = 0; i < result.length; i++) {
-                    const element = result[i];
-                    let newPlace = new Place();
-                    newPlace.placeId = element.place_id;
-
-                    try {
-                        newPlace = await AppDataSource.manager.save(newPlace);
-                    } catch (e) {
-                        console.log(e);
-                    }
-                }
             })
             .catch((e) => {
                 errorMessage = e.response.data.error_message;
@@ -89,49 +52,51 @@ export async function getAutocompleteResult(req: Request, res: Response) {
 
 export async function getPlaceDetails(req: Request, res: Response) {
     const placeId = req.params.placeId as string;
-    let meta: MetaData;
-    let result: Partial<PlaceData>;
+    let output: Place;
     let errorMessage: any;
     let status = 200;
 
-    if (placeId && typeof placeId === "string") {
-        await client
-            .placeDetails({
-                params: {
-                    place_id: placeId,
-                    key: process.env.GOOGLEAPI_KEY,
-                    fields: detailFields,
-                },
-            })
-            .then(async (response) => {
-                result = response.data.result;
+    if (placeId) {
+        const dbPlace = await AppDataSource.getRepository(Place)
+            .createQueryBuilder("places")
+            .leftJoinAndSelect("places.tags", "tags")
+            .where({ placeId })
+            .getOne();
 
-                if (result.website) {
-                    meta =
-                        (await getWebsiteInfo(result.website).catch(
-                            (e: Error) => {
-                                console.log(e);
-                                errorMessage = e.message;
-                                status = 500;
-                            }
-                        )) || undefined;
-                }
-            })
-            .catch((e) => {
-                errorMessage = e.response.data.error_message;
-                console.log(e.response.data.error_message);
-                status = 500;
-            });
+        if (dbPlace) {
+            output = dbPlace;
+        } else {
+            await client
+                .placeDetails({
+                    params: {
+                        place_id: placeId,
+                        key: process.env.GOOGLEAPI_KEY,
+                        fields: detailFields,
+                    },
+                })
+                .then(async (response) => {
+                    const place = new Place();
+                    const googleResult = response.data.result;
+
+                    place.placeId = googleResult.place_id;
+                    place.address = googleResult.formatted_address;
+                    place.name = googleResult.name;
+                    place.tags = [];
+
+                    output = await AppDataSource.manager.save(place);
+                })
+                .catch((e) => {
+                    errorMessage = e.response.data.error_message;
+                    console.log(e.response.data.error_message);
+                    status = 500;
+                });
+        }
     } else {
         status = 422;
     }
 
     if (status === 200) {
-        if (meta) {
-            res.send({ ...result, meta });
-        } else {
-            res.send({ ...result });
-        }
+        res.send(output);
     } else if (status === 500) {
         res.status(status);
         res.send(errorMessage);
